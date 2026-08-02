2baae08ef7b92e7f6a678bc5b96c62826c80ed3e
Basma_Mokhles <bbmm002017@gmail.com>
2026-08-02 13:56:13 +0300

    update: finalize admin dashboard with database integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import { LoginPage } from "./pages/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage";
 import CourseWizardPage from "./pages/CourseWizardPage";
 import LessonEditorPage from "./pages/LessonEditorPage";
+import AdminDashboard from "./pages/adminDashboard";
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
         <Route path="/instructor/courses/new" element={<CourseWizardPage />} />
         <Route path="/instructor/courses/:courseId/edit" element={<CourseWizardPage />} />
         <Route path="/instructor/courses/:courseId/lessons" element={<LessonEditorPage />} />
+        <Route path="/admin" element={<AdminDashboard />} />
       </Routes>
     </BrowserRouter>
   );
